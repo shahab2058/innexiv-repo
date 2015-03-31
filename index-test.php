ce02a54820558e08d95d4f46497513e69ef396ca
shahab2058 <shahab2058@users.noreply.github.com>
2015-03-31 19:26:03 +0500

    Update index-test.php

--- index-test.php
+++ index-test.php
@@ -12,4 +12,4 @@ $config=dirname(__FILE__).'/protected/config/test.php';
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
 
 require_once($yii);
-Yii::createWebApplication($config)->run();
+Yii::createWebApplication($config)->run();s
